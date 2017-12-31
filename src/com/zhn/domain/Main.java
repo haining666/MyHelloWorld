24bb848bfb8058c83812215655dd93158f69145a
zhanghaining <254480062@qq.com>
2017-12-31 13:09:39 +0800

    Initial commit

---
+++ src/com/zhn/domain/Main.java
@@ -0,0 +1,8 @@
+package com.zhn.domain;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("zhanghaining");
+        System.out.println("我叫张海宁");
+    }
+}
